7f1c235c27e103e362ad801e46d25832e49fd055
ThePresistence <102717267+ThePresistence@users.noreply.github.com>
2023-11-25 17:25:59 +0000

    added everything

--- everything/app.py
+++ everything/app.py
@@ -9,17 +9,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-
 def create_database(github_link): 
-    process = subprocess.Popen(['python', 'src/main.py', 'process', '--repo-url', github_link])
+    process = subprocess.Popen(['python', 'main.py', 'process', '--repo-url', github_link])
     process.wait()
     print('done')
 
     dataset_name = github_link.split('/')[-1]
 
     return dataset_name 
-
 
 def search_db(db, query):
     """Search for a response to the query in the DeepLake database."""
@@ -36,6 +33,7 @@ def search_db(db, query):
     qa = RetrievalQA.from_llm(model, retriever=retriever)
     # Return the result of the query
     return qa.run(query)
+
 
 def ask_multiple_question_and_save_it_in_text_file(dataset_name):
     inputs_questions = ["What is the aim of the project? ", 
@@ -72,54 +70,4 @@ def ask_multiple_question_and_save_it_in_text_file(dataset_name):
             output = search_db(db, user_input)
             print(output)
             all_outputs.append(output)
-            file.write(output)
-  
-
-# database_name = create_database("https://github.com/rpgeeganage/alls")
-# out = ask_multiple_question_and_save_it_in_text_file(database_name)
-
-import os, openai, langchain
-
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferMemory
-from langchain.llms.base import LLM
-
-
-# !pip install openai
-import os
-from openai import OpenAI
-
-gpt_4_key = "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"
-os.environ["OPENAI_API_KEY"] = gpt_4_key
-client = OpenAI()
-
-## Funciton for read me maker
-def give_answer_readme(content):
-  completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "system", "content": "Pretend you are README file creator for github. I have provided the aim of the project, data resources, workflow diagram, Insights about the project, deployment and insights for the developers. Take a detailed note of everything and create a markdown file out of it. That markdown file needs to understand by the developers, write the content in very easy terms and understandable. I need the ouput in markdown file"},
-      {"role": "user", "content": content}
-    ]
-  )
-
-  return completion.choices[0].message.content
-
-
-def give_answer_transcription_video(content):
-
-  completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "system", "content": "You're a video creator expert in explaining the project to developers. User will give you the aim, workflow diagram, deployment details, data resrouces, insights of the project and additional information of the project. By using this information create for 2 min. Explain all the information properly."},
-      {"role": "user", "content": content}
-    ]
-  )
-
-  return completion.choices[0].message.content
-
-
-with open("all_output.txt", 'w') as file: 
-    file.write("ohhh")
+            file.write(output) 
---
+++ everything/streamlits.py
@@ -0,0 +1,4 @@
+from app import * 
+
+# database_name = create_database("https://github.com/eliasku/unit")
+out = ask_multiple_question_and_save_it_in_text_file("unit")
